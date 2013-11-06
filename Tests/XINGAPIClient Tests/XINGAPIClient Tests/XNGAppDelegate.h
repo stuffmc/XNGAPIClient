edd466f10998c6aca70b22f962af0f1be9630c89
Stefan Munz <stefan.munz@xing.com>
2013-11-06 11:15:49 +0100

    add empty test project.

---
+++ Tests/XINGAPIClient Tests/XINGAPIClient Tests/XNGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XNGAppDelegate.h
+//  XINGAPIClient Tests
+//
+//  Created by Stefan Munz on 11/6/13.
+//  Copyright (c) 2013 XING AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tests/XINGAPIClient Tests/XINGAPIClient Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XINGAPIClient Tests
+//
+//  Created by Stefan Munz on 11/6/13.
+//  Copyright (c) 2013 XING AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XNGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XNGAppDelegate class]));
+    }
+}
