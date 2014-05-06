7b71d1cd5b94222613e689e73e6698e337b9116f
Piet Brauer <piet@nerdishbynature.com>
2014-05-06 16:09:04 +0200

    Recreate from pod template

---
+++ Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Piet Brauer on 06.05.14.
+//  Copyright (c) 2014 XING AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XNGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XNGAppDelegate class]));
+    }
+}
--- Tests/XINGAPIClient Tests App/XNGAppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface XNGAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
